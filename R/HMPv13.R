ecfd2c639bb6aa51f8388fd1ae8a572d399905f8
Tom B <tb1280@nyu.edu>
2017-01-24 16:46:57 -0500

    Add HMPv13
    Add TravisCI

---
+++ R/HMPv13.R
@@ -0,0 +1,10 @@
+#' @title HMP 16S rRNA data from V1-V3
+#' @description This dataset contains 16S rRNA amplicon data from the HMP Project.
+#' @usage data('HMPv13')
+#' @docType data
+#' @format An object of class \code{"phyloseq"}.
+#' @keywords datasets
+#' @references The NIH HMP Working Group et al. (2009) Genome Res 19(12): 2317–2323.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2792171}{PubMed})
+#'
+"HMPv13"
